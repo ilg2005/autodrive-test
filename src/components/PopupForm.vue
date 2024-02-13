4dce91bd01fdac4027bb2aa73aa8a5760b78fa51
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2024-02-13 22:01:37 +0300

    form fields validations

--- src/components/PopupForm.vue
+++ src/components/PopupForm.vue
@@ -1,21 +1,73 @@
 <script setup>
-import { onBeforeMount, ref } from 'vue'
+import { onBeforeMount, reactive, ref } from 'vue'
 import ButtonPopupClose from '@/components/ButtonPopupClose.vue'
 import store from '@/store/index.js'
 
 const title = 'Заполните форму'
+const form = ref(null);
 const name = ref('')
 const phone = ref('')
 const email = ref('')
 const city = ref('')
 const cities = ref([])
 
+let formErrors = reactive({})
+
 onBeforeMount(() => {
   cities.value = store.getters.getCities;
   city.value = store.getters.getCurrentCityId;
 })
 
+function cleanPhoneNumber (phone) {
+  return phone.replace(/\D/g, '');
+}
+function isValidPhoneNumber(phone) {
+  const cleaned = cleanPhoneNumber(phone);
+  return /^\d+$/.test(cleaned);
+}
+
+function validateForm() {
+
+  if (!name.value) {
+    formErrors.name = 'Обязательное поле'
+  } else {
+    formErrors.name = '';
+  }
+
+  if (!phone.value) {
+    formErrors.phone = 'Обязательное поле'
+  } else if (!isValidPhoneNumber(phone.value)) {
+    formErrors.phone = 'Введите валидный телефон'
+  } else {
+    formErrors.phone = '';
+  }
+
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email.value) {
+    formErrors.email = 'Обязательное поле';
+  } else if (!emailRegex.test(email.value)) {
+    formErrors.email = 'Введите действительный email';
+  } else {
+    formErrors.email = '';
+  }
+
+  if (!city.value) {
+    formErrors.city = 'Обязательное поле'
+  } else {
+    formErrors.city = '';
+  }
+
+  return Object.keys(formErrors).every(key => !formErrors[key]);
+}
 function submitForm() {
+
+   if (validateForm()) {
+     console.log('name:', name.value);
+     console.log('phone:', phone.value);
+     console.log('name:', email.value);
+     console.log('name:', city.value);
+   }
+
   // Handle form submission logic here
 }
 
@@ -30,32 +82,36 @@ function submitForm() {
         <ButtonPopupClose />
       </div>
 
-      <form @submit.prevent="submitForm">
+      <form ref="form" @submit.prevent="submitForm" novalidate>
         <div class="mb-4">
           <label class="block mb-2">Имя:
-            <input v-model="name" class="border p-2 w-full" name="name" type="text" />
+            <input v-model.trim="name" class="border p-2 w-full" name="name" type="text" />
           </label>
+          <span class="text-red-500">{{ formErrors.name }}</span>
         </div>
 
         <div class="mb-4">
           <label class="block mb-2">Телефон:
-            <input v-model="phone" class="border p-2 w-full" name="phone" type="tel" />
+            <input v-model.trim="phone" class="border p-2 w-full" name="phone" type="tel" />
           </label>
+          <span class="text-red-500">{{ formErrors.phone }}</span>
         </div>
 
         <div class="mb-4">
           <label class="block mb-2">Email:
-            <input v-model="email" class="border p-2 w-full" name="email" type="email" />
+            <input v-model.trim="email" class="border p-2 w-full" name="email" type="email" />
           </label>
+          <span class="text-red-500">{{ formErrors.email }}</span>
         </div>
 
         <div class="mb-4">
           <label class="block mb-2">Город:
-            <select v-model="city" class="border p-2 w-full" name="city_id">
+            <select v-model.trim="city" class="border p-2 w-full" name="city_id">
               <option v-for="cityOption in cities" :key="cityOption" :value="cityOption.id">{{ cityOption.name }}
               </option>
             </select>
           </label>
+          <span class="text-red-500">{{ formErrors.city }}</span>
         </div>
 
         <button class="btn blue mt-6 w-full rounded" type="submit">Отправить</button>
