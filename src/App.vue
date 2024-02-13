3602407df964252ec99aa216f3ed60baa7bc7bc4
ilg2005 <36738148+ilg2005@users.noreply.github.com>
2024-02-13 15:21:35 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+
+  </header>
+
+  <main>
+    <h1 class="text-3xl font-bold underline">
+      Test
+    </h1>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
